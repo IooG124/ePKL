baea6c3831ea6cdf2bddc7a76714d1f7734dd723
DiooG <139132357+dioputra1423@users.noreply.github.com>
2024-11-21 08:28:28 +0800

    Create Models

---
+++ app/Models/Siswa.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    public function user()
+    {
+        return $this->morphOne(User::class, 'userable');
+    }
+
+    public function jurnal()
+    {
+        return $this->morphMany(Jurnal::class, 'author');
+    }
+}
