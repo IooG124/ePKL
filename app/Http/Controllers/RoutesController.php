f3388aabdb4774dd957181bc7045c4e47d9c346d
Segara <shinkyuzu2445@gmail.com>
2025-01-31 15:31:55 +0800

    Resolved merge conflicts in RoutesController

--- app/Http/Controllers/RoutesController.php
+++ app/Http/Controllers/RoutesController.php
@@ -2,36 +2,40 @@
 
 namespace App\Http\Controllers;
 
-class RoutesController extends Controller{
-      public function vLogin(){
-            return view('login');
-      }
-      
-      public function vAbsen(){
-            return view('absen');
-      }
+class RoutesController extends Controller {
+    public function vLogin() {
+        return view('login');
+    }
+    
+    public function vAbsen() {
+        return view('absen');
+    }
 
-      public function vDSiswa(){
-            return view('tsiswa');
-      }
+    public function vDSiswa() {
+        return view('tsiswa');
+    }
 
-      public function vDGuru(){
-            return view('tguru');
-      }
+    public function vDGuru() {
+        return view('tguru');
+    }
 
-      public function vDUDI(){
-            return view('ddudi');
-      }
+    public function vDUDI() {
+        return view('ddudi');
+    }
 
-      public function vJurnal(){
-            return view('jurnal');
-      }
+    public function vJurnal() {
+        return view('jurnal');
+    }
 
-      public function vProfile(){
-            return view('Profile');
-      }
+    public function vProfile() {
+        return view('Profile');
+    }
 
-      public function vVerifikasi(){
-            return view('verif');
-      }
+    public function vVerifikasi() {
+        return view('verif');
+    }
+
+    public function vScan() {
+        return view('scanface');
+    }
 }
